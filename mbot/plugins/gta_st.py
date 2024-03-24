23769a61e958dee2155b0a69c45f6d1608b2bd32
Hydra <119828208+Hydra-sjz@users.noreply.github.com>
2024-03-24 17:02:39 +0530

    Create gta_st.py

---
+++ mbot/plugins/gta_st.py
@@ -0,0 +1,34 @@
+import os
+from pyrogram import Client, filters
+from telegraph import upload_file
+
+
+@Client.on_message(filters.command(["wasted"]))
+async def wasted(bot, message):
+    gta = await bot.send_message(message.chat.id, "`Processing...`")
+    if not message.reply_to_message:
+        await gta.edit("`Reply to a photo :(`")
+        return
+    ok = message.reply_to_message
+    pic = await bot.download_media(ok)
+    poto_url = upload_file(pic)
+    imglink = f"https://telegra.ph{poto_url[0]}"
+    url = f"https://some-random-api.com/canvas/wasted?avatar={imglink}"
+    await message.reply_photo(url)
+    await gta.delete()
+    os.remove(pic)
+
+@Client.on_message(filters.command(["passed"]))
+async def mission_passed(bot, message):
+    gta = await bot.send_message(message.chat.id, "`Processing...`")
+    if not message.reply_to_message:
+        await gta.edit("`Reply to a photo :(`")
+        return
+    ok = message.reply_to_message
+    pic = await bot.download_media(ok)
+    poto_url = upload_file(pic)
+    imglink = f"https://telegra.ph{poto_url[0]}"
+    url = f"https://some-random-api.com/canvas/overlay/passed?avatar={imglink}"
+    await message.reply_photo(url)
+    await gta.delete()
+    os.remove(pic)
