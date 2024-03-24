33c8cf412f64b2e6f589d4726778798ee7a227c0
Hydra <119828208+Hydra-sjz@users.noreply.github.com>
2024-03-24 18:54:41 +0530

    Create stcr.py

---
+++ mbot/plugins/stcr.py
@@ -0,0 +1,52 @@
+import glob
+import io
+import os
+import random
+import textwrap
+
+from PIL import Image, ImageDraw, ImageFont
+from pyrogram import Client, filters
+
+
+@Client.on_message(filters.command(["stcr"]))
+async def make_stcr(c, m):
+    ok = await m.reply_text("**Making A Cool StiCkeR**")
+    R = random.randint(0, 256)
+    G = random.randint(0, 256)
+    B = random.randint(0, 256)
+    text = None
+    if m.reply_to_message:
+        if m.reply_to_message.caption:
+            text = m.reply_to_message
+        elif m.reply_to_message.text:
+            text = m.reply_to_message.text
+    elif len(m.command) > 1:
+        text = m.text.split(" ", 1)[1]
+    if not text:
+        return await ok.edit("`Give some input to create a sticker...`")
+    sticktext = textwrap.wrap(text, width=10)
+    sticktext = "\n".join(sticktext)
+    image = Image.new("RGBA", (512, 512), (255, 255, 255, 0))
+    draw = ImageDraw.Draw(image)
+    fontsize = 230
+    fpath = glob.glob("resources/Fonts/*")
+    fonts = random.choice(fpath)
+    font = ImageFont.truetype(fonts, size=fontsize)
+    while draw.multiline_textsize(sticktext, font=font) > (512, 512):
+        fontsize -= 3
+        font = ImageFont.truetype(fonts, size=fontsize)
+    width, height = draw.multiline_textsize(sticktext, font=font)
+    draw.multiline_text(
+        ((512 - width) / 2, (512 - height) / 2), sticktext, font=font, fill=(R, G, B)
+    )
+    image_stream = io.BytesIO()
+    image_stream.name = "@fStark.webp"
+    image.save(image_stream, "WebP")
+    image_stream.seek(0)
+    await m.reply_document(image_stream, caption="Mr.Stark")
+    await ok.delete()
+    try:
+        os.remove(image)
+        os.remove(image_stream)
+    except:
+        pass
