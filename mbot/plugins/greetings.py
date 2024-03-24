eb29df433c97ed956351553cc4c606ad292f3fd2
Hydra <119828208+Hydra-sjz@users.noreply.github.com>
2024-03-24 17:24:32 +0530

    Update greetings.py

--- mbot/plugins/greetings.py
+++ mbot/plugins/greetings.py
@@ -26,6 +26,9 @@ startbt = InlineKeyboardMarkup(
             ]
         ]
 )
+stk = (
+    "CAACAgUAAxkBAAEC6JVmAAETqSfP_73ZK2lF5UBjikWA4WkAApMPAAJYPgABVK6vQdKgxIntHgQ",
+)
 @Mbot.on_message(filters.private)
 async def _(bot, cmd):
     await handle_user_status(bot, cmd)
@@ -46,6 +49,7 @@ async def start_command(bot, message):
         text=start_cmd.format(message.from_user.first_name), 
         reply_markup=startbt,
     )
+    await message.reply_sticker(stk)
     await message.delete()
 #==================≠
 
