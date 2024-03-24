811d43c3c4102688834133910c0deaf21df40176
Hydra <119828208+Hydra-sjz@users.noreply.github.com>
2024-03-24 15:24:31 +0530

    Create human_read.py

---
+++ mbot/utils/helper/human_read.py
@@ -0,0 +1,19 @@
+def get_readable_time2(seconds: int) -> str:
+    count = 0
+    ping_time = ""
+    time_list = []
+    time_suffix_list = ["s", "m", "h", "d", "w", "m", "y"]
+    while count < 4:
+        count += 1
+        remainder, result = divmod(seconds, 60) if count < 3 else divmod(seconds, 24)
+        if seconds == 0 and remainder == 0:
+            break
+        time_list.append(int(result))
+        seconds = int(remainder)
+    for i in range(len(time_list)):
+        time_list[i] = str(time_list[i]) + time_suffix_list[i]
+    if len(time_list) == 4:
+        ping_time += f"{time_list.pop()}, "
+    time_list.reverse()
+    ping_time += ":".join(time_list)
+    return ping_time
