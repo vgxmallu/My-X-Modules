b3a3fe03d88fdf9642b5667545a2c2b61c9abc70
Hydra <119828208+Hydra-sjz@users.noreply.github.com>
2024-03-24 14:22:29 +0530

    Update config.py

--- config.py
+++ config.py
@@ -2,6 +2,12 @@ import os
 import time
 from os import environ
 
-#MOMGO DATABASE
+#MOMGO_DATABASE
 DB_URL = os.environ.get("DB_URL", "")
 DB_NAME = os.environ.get("DB_NAME", "")
+
+#LOGS_CHANNEL
+LOG_CHANNEL = int(os.environ.get("LOG_CHANNEL", "-1001784386455"))
+
+#AUTHENTICATED_USERS
+AUTH_USERS = set(int(x) for x in os.environ.get("AUTH_USERS", "784589736").split())
