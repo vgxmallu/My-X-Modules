ba5005ac97513f6b238e3f4a289820b4537a90d3
Hydra <119828208+Hydra-sjz@users.noreply.github.com>
2024-03-24 15:25:16 +0530

    Create utils.py

---
+++ mbot/utils/helper/utils.py
@@ -0,0 +1,14 @@
+from datetime import datetime, timedelta
+#from mbot import cleanmode
+
+cleanmode = {}
+
+async def put_cleanmode(chat_id, message_id):
+    if chat_id not in cleanmode:
+        cleanmode[chat_id] = []
+    time_now = datetime.now()
+    put = {
+        "msg_id": message_id,
+        "timer_after": time_now + timedelta(minutes=1),
+    }
+    cleanmode[chat_id].append(put)
