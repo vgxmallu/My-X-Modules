9e136e180787b13edce3f46ab0cf79eb7cc14011
Hydra <119828208+Hydra-sjz@users.noreply.github.com>
2024-03-25 22:22:03 +0530

    Update currency.py

--- mbot/plugins/currency.py
+++ mbot/plugins/currency.py
@@ -14,15 +14,12 @@ LOGGER = logging.getLogger("X_Mod")
 @Mbot.on_message(filters.command("currency"))
 async def currency(_, ctx: Message):
     if CURRENCY_API is None:
-        return await ctx.reply_msg(
+        return await ctx.reply_message(
             "<code>Oops!!get the API from</code> <a href='https://app.exchangerate-api.com/sign-up'>HERE</a> <code>& add it to config vars</code> (<code>CURRENCY_API</code>)",
             disable_web_page_preview=True,
         )
     if len(ctx.text.split()) != 4:
-        return await ctx.reply_msg(
-            f"Use format /{ctx.command[0]} [amount] [currency_from] [currency_to] to convert currency.",
-            del_in=6,
-        )
+        return await ctx.reply_message(f"Use format /{ctx.command[0]} [amount] [currency_from] [currency_to] to convert currency.")
 
     _, amount, currency_from, currency_to = ctx.text.split()
     if amount.isdigit() or (
@@ -42,15 +39,15 @@ async def currency(_, ctx: Message):
                 base_code = data["base_code"]
                 last_update = data["time_last_update_utc"]
             except KeyError:
-                return await ctx.reply_msg("<code>Invalid response from api !</i>")
-            await ctx.reply_msg(
+                return await ctx.reply_message("<code>Invalid response from api !</i>")
+            await ctx.reply_message(
                 f"**CURRENCY EXCHANGE RATE RESULT:**\n\n`{format(float(amount), ',')}` **{base_code}** = `{format(float(conversion_result), ',')}` **{target_code}**\n<b>Rate Today</b> = `{format(float(conversion_rate), ',')}`\n<b>Last Update:</b> {last_update}"
             )
         except Exception as err:
-            await ctx.reply_msg(
+            await ctx.reply_message(
                 f"Failed convert currency, maybe you give wrong currency format or api down.\n\n<b>ERROR</b>: {err}"
             )
     else:
-        await ctx.reply_msg(
+        await ctx.reply_message(
             "<code>This seems to be some alien currency, which I can't convert right now.. (⊙_⊙;)</code>"
         )
