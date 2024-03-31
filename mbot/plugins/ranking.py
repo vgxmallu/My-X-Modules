e6cf33b50e4a4f35c7861d8769114c8e29c07c02
Hydra <119828208+Hydra-sjz@users.noreply.github.com>
2024-03-31 11:05:20 +0530

    Update ranking.py

--- mbot/plugins/ranking.py
+++ mbot/plugins/ranking.py
@@ -1,160 +1,125 @@
-from mbot.utils.Database.rankdb import get_name, increase_count, chatdb
 
-from pyrogram.client import Client
-from pyrogram import filters
-from datetime import date
-from pyrogram.types import (
-    Message,
-    CallbackQuery,
-    InlineKeyboardMarkup,
-    InlineKeyboardButton,
+from mbot import Mbot as bot
+from pyrogram import Client , filters
+from pymongo import MongoClient
+import os
+from config import DB_URL
+
+async def is_admins(chat_id: int):
+    return [
+        member.user.id
+        async for member in bot.iter_chat_members(
+            chat_id, filter="administrators"
+        )
+    ]
+
+levellink =["https://telegra.ph/file/b507aa1334caacbbbf3b5.mp4", "https://telegra.ph/file/028fb314488a82422d85d.mp4", "https://telegra.ph/file/cca115d41d2c90edaa0be.mp4", "https://telegra.ph/file/a74c0dfc2fd656cf11c64.mp4", "https://telegra.ph/file/81e09bea9b389166f5a70.mp4", "https://telegra.ph/file/afd7ad7349c5382e206f4.mp4", "https://telegra.ph/file/8713be38beb9e1d3a2969.mp4", "https://telegra.ph/file/86ffec208e702d7634506.mp4"]
+levelname = ["Team Beginner", "Stray Trainee", "Vector Amateur", "Hero Expert", "Z Semi-Pro", "Professional", "Champ", "⚔️ Immortal 🗡️"]
+levelnum = [2,5,15,25,35,50,70,100]
+
+
+
+@bot.on_message(
+    filters.command("level", prefixes=["/", ".", "?", "-"])
 )
-
-from mbot import Mbot as app
-
-
-@app.on_message(
-    ~filters.bot
-    & ~filters.forwarded
-    & filters.group
-    & ~filters.via_bot
-    & ~filters.service
-)
-async def inc_user(_, message: Message):
-    if message.text:
-        if (
-            message.text.strip() == "/top@RankingssBot"
-            or message.text.strip() == "/top"
-        ):
-            return await show_top_today(_, message)
-        if (
-            message.text.strip() == "/start@RankingssBot"
-            or message.text.strip() == "/start"
+async def levelsystem(_, message): 
+    leveldb = MongoClient(DB_URL)
+   
+    toggle = leveldb["ToggleDb"]["Toggle"] 
+    if message.from_user:
+        user = message.from_user.id
+        chat_id = message.chat.id
+        if user not in (
+            await is_admins(chat_id)
         ):
             return await message.reply_text(
-                "**Hi, I am a ranking bot.**\n\nI can rank the top 10 users in a chat based on the number of messages they have sent.\n\nClick /top to see the top 10 users in this chat."
+                "You are not admin"
             )
+    is_level = toggle.find_one({"chat_id": message.chat.id})
+    if not is_level:
+        toggle.insert_one({"chat_id": message.chat.id})
+        await message.reply_text("Level System Enable")
+    else:
+        toggle.delete_one({"chat_id": message.chat.id})
+        await message.reply_text("Level System Disable")
 
+
+@bot.on_message(
+    (filters.document
+     | filters.text
+     | filters.photo
+     | filters.sticker
+     | filters.animation
+     | filters.video)
+    & ~filters.private,
+    group=8,
+)
+async def level(client, message):
     chat = message.chat.id
-    user = message.from_user.id
-    increase_count(chat, user)
-    print(chat, user, "increased")
+    user_id = message.from_user.id    
 
-
-async def show_top_today(_, message: Message):
-    print("today top in", message.chat.id)
-    chat = chatdb.find_one({"chat": message.chat.id})
-    today = str(date.today())
-
-    if not chat:
-        return await message.reply_text("no data available",
-        reply_markup=InlineKeyboardMarkup(
-            [[InlineKeyboardButton("Overall Ranking", callback_data="overall")]]
-        ),)
-
-    if not chat.get(today):
-        return await message.reply_text("no data available for today",
-        reply_markup=InlineKeyboardMarkup(
-            [[InlineKeyboardButton("Overall Ranking", callback_data="overall")]]
-        ),)
-
-    t = "🔰 **Today's Top Users :**\n\n"
-
-    pos = 1
-    for i, k in sorted(chat[today].items(), key=lambda x: x[1], reverse=True)[:10]:
-        i = await get_name(app, i)
-
-        t += f"**{pos}.** {i} - {k}\n"
-        pos += 1
-
-    total = sum(chat[today].values())
-    t += f'\n✉️ Today messages: {total}'
-
-    await message.reply_text(
-        t,
-        reply_markup=InlineKeyboardMarkup(
-            [[InlineKeyboardButton("Overall Ranking", callback_data="overall")]]
-        ),
-    )
-
-
-@app.on_callback_query(filters.regex("overall"))
-async def show_top_overall_callback(_, query: CallbackQuery):
-    print("overall top in", query.message.chat.id)
-    chat = chatdb.find_one({"chat": query.message.chat.id})
-
-    if not chat:
-        return await query.answer("No data available", show_alert=True)
-
-    await query.answer("Processing... Please wait")
-
-    t = "🔰 **Overall Top Users :**\n\n"
-
-    overall_dict = {}
-    total =0
-    for i, k in chat.items():
-        if i == "chat" or i == "_id":
-            continue
-
-        for j, l in k.items():
-            if j not in overall_dict:
-                overall_dict[j] = l
-            else:
-                overall_dict[j] += l
-
-        total += sum(k.values())
+    leveldb = MongoClient(DB_URL)
     
+    level = leveldb["LevelDb"]["Level"] 
+    toggle = leveldb["ToggleDb"]["Toggle"] 
 
-    pos = 1
-    for i, k in sorted(overall_dict.items(), key=lambda x: x[1], reverse=True)[:10]:
-        i = await get_name(app, i)
+    is_level = toggle.find_one({"chat_id": message.chat.id})
+    if is_level:
+        xpnum = level.find_one({"level": user_id, "chatid": chat})
 
-        t += f"**{pos}.** {i} - {k}\n"
-        pos += 1
+        if not message.from_user.is_bot:
+            if xpnum is None:
+                newxp = {"level": user_id, "chatid": chat, "xp": 10}
+                level.insert_one(newxp)   
+                    
+            else:
+                xp = xpnum["xp"] + 10
+                level.update_one({"level": user_id, "chatid": chat}, {
+                    "$set": {"xp": xp}})
+                l = 0
+                while True:
+                    if xp < ((50*(l**2))+(50*(l))):
+                         break
+                    l += 1
+                xp -= ((50*((l-1)**2))+(50*(l-1)))
+                if xp == 0:
+                    await message.reply_text(f"🌟 {message.from_user.mention}, You have reached level {l}**, Nothing can stop you on your way!")
+    
+                    for lv in range(len(levelname)) and range(len(levellink)):
+                            if l == levelnum[lv]:            
+                                Link = f"{levellink[lv]}"
+                                await message.reply_video(video=Link, caption=f"{message.from_user.mention}, You have reached Rank Name **{levelname[lv]}**")
+                  
 
-    t += f'\n✉️ Today messages: {total}'
+                               
+@bot.on_message(
+    filters.command("rank", prefixes=["/", ".", "?", "-"])
+    & ~filters.private)
+async def rank(client, message):
+    chat = message.chat.id
+    user_id = message.from_user.id    
+    
+    leveldb = MongoClient(DB_URL)
+    
+    level = leveldb["LevelDb"]["Level"] 
+    toggle = leveldb["ToggleDb"]["Toggle"] 
 
-    await query.message.edit_text(
-        t,
-        reply_markup=InlineKeyboardMarkup(
-            [[InlineKeyboardButton("Today's Ranking", callback_data="today")]]
-        ),
-    )
+    is_level = toggle.find_one({"chat_id": message.chat.id})
+    if is_level:
+        xpnum = level.find_one({"level": user_id, "chatid": chat})
+        xp = xpnum["xp"]
+        l = 0
+        r = 0
+        while True:
+            if xp < ((50*(l**2))+(50*(l))):
+                break
+            l += 1
 
-
-@app.on_callback_query(filters.regex("today"))
-async def show_top_today_callback(_, query: CallbackQuery):
-    print("today top in", query.message.chat.id)
-    chat = chatdb.find_one({"chat": query.message.chat.id})
-    today = str(date.today())
-
-    if not chat:
-        return await query.answer("No data available", show_alert=True)
-
-    if not chat.get(today):
-        return await query.answer("No data available for today", show_alert=True)
-
-    await query.answer("Processing... Please wait")
-
-    t = "🔰 **Today's Top Users :**\n\n"
-
-    pos = 1
-    for i, k in sorted(chat[today].items(), key=lambda x: x[1], reverse=True)[:10]:
-        i = await get_name(app, i)
-
-        t += f"**{pos}.** {i} - {k}\n"
-        pos += 1
-
-
-    total = sum(chat[today].values())
-    t += f'\n✉️ Today messages: {total}'
-
-
-    await query.message.edit_text(
-        t,
-        reply_markup=InlineKeyboardMarkup(
-            [[InlineKeyboardButton("Overall Ranking", callback_data="overall")]]
-        ),
-    )
-
+        xp -= ((50*((l-1)**2))+(50*(l-1)))
+        rank = level.find().sort("xp", -1)
+        for k in rank:
+            r += 1
+            if xpnum["level"] == k["level"]:
+                break                     
+        await message.reply_text(f"{message.from_user.mention} Level Info:\nLevel: {l}\nProgess: {xp}/{int(200 *((1/2) * l))}\n Ranking: {r}")
 
