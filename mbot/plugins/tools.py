7bc94c24a623d9a412f380b51e83c73af2f73967
Hydra <119828208+Hydra-sjz@users.noreply.github.com>
2024-04-01 07:49:01 +0530

    Update tools.py

--- mbot/plugins/tools.py
+++ mbot/plugins/tools.py
@@ -100,12 +100,12 @@ def send_upload_progress(chat_id, message_id):
     return callback
 
 
-def download_audio(url,chat_id):
-    send_message(chat_id, "Audio download started...")
+def download_audio(url):
+    await message.reply_message(chat_id, "Audio download started...")
     with audio_ydl as ydl:
         info = ydl.extract_info(url)
         filename = ydl.prepare_filename(info)
-        send_message(chat_id, "Audio download finished!")
+        await message.reply_message("Audio download finished!")
         return filename
 
 
