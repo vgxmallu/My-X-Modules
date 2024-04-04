d6904d1e5908dfe262326893542ccac918c89ae1
Hydra <119828208+Hydra-sjz@users.noreply.github.com>
2024-04-04 21:38:13 +0530

    Update ai.py

--- mbot/plugins/ai.py
+++ mbot/plugins/ai.py
@@ -1,5 +1,6 @@
 import os, time
 import openai
+import random
 from pyrogram import filters, Client
 import requests
 from mbot import Mbot as app
@@ -23,6 +24,38 @@ API_URL = "https://sugoi-api.vercel.app/search"
 
 
 
+@app.on_message(filters.command(["chatgpt3","ai","ask","gpt","solve"],  prefixes=["/"]))
+async def chat_gpt(bot, message):
+    try:
+        start_time = time.time()
+        await bot.send_chat_action(message.chat.id, ChatAction.TYPING)
+
+        if len(message.command) < 2:
+            await message.reply_text(
+                "Example:\n\n/chatgpt Where is TajMahal?"
+            )
+        else:
+            a = message.text.split(' ', 1)[1]
+            response = requests.get(f'https://chatgpt.apinepdev.workers.dev/?question={a}')
+
+            try:
+                # Check if "results" key is present in the JSON response
+                if "answer" in response.json():
+                    x = response.json()["answer"]
+                    end_time = time.time()
+                    telegram_ping = str(round((end_time - start_time) * 1000, 3)) + " ms"
+                    await message.reply_text(
+                        f" {x}      ᴀɴsᴡᴇʀɪɴɢ ʙʏ ➛  @GojoSatoru_Xbot",
+                        parse_mode=ParseMode.MARKDOWN
+                    )
+                else:
+                    await message.reply_text("No 'results' key found in the response.")
+            except KeyError:
+                # Handle any other KeyError that might occur
+                await message.reply_text("Error accessing the response.")
+    except Exception as e:
+        await message.reply_text(f"**á´‡Ê€Ê€á´Ê€: {e} ")
+
 api_url_gpt = "https://nandha-api.onrender.com/ai/gpt"
 api_url_bard = "https://nandha-api.onrender.com/ai/bard"
 
@@ -37,7 +70,7 @@ def fetch_data(api_url: str, query: str) -> tuple:
     except Exception as e:
         return None, f"An error occurred: {str(e)}"
 
-@app.on_message(filters.command(["tchat", "task", "gput"]))
+@app.on_message(filters.command(["chatg1", "task", "gput"]))
 async def chatgptt(_, message):
     if len(message.command) < 2:
         return await message.reply_text("**Please provide a query.**")
@@ -105,7 +138,7 @@ async def bardb(_, message):
 
 
 # ----------------------------------------
-@app.on_message(filters.command(["chatgpt","ai","ask"],  prefixes=["+", ".", "/", "-", "?", "$","#","&"]))
+@app.on_message(filters.command(["chatgpt2","ai","ask"],  prefixes=["+", ".", "/", "-", "?", "$","#","&"]))
 async def chat(app :app, message):
     
     try:
