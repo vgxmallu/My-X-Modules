6084961419b43a88337ffc0d2b1cc23cde70d045
Hydra <119828208+Hydra-sjz@users.noreply.github.com>
2024-04-04 21:14:58 +0530

    Update ai.py

--- mbot/plugins/ai.py
+++ mbot/plugins/ai.py
@@ -1,12 +1,16 @@
 import os, time
 import openai
-from pyrogram import filters
+from pyrogram import filters, Client
+import requests
 from mbot import Mbot as app
 from pyrogram.enums import ChatAction, ParseMode
 from gtts import gTTS
 import requests
 import requests as r
 from config import GPT_API, DEEP_API
+from pyrogram.errors import MediaCaptionTooLong
+
+from pyrogram.types import Message, InputMediaPhoto
 # ----------------------------------------
 # ----------------------------------------
 openai.api_key = GPT_API
@@ -16,6 +20,90 @@ api_key = DEEP_API
 API_URL = "https://sugoi-api.vercel.app/search"
 
 # ----------------------------------------
+
+
+
+api_url_gpt = "https://nandha-api.onrender.com/ai/gpt"
+api_url_bard = "https://nandha-api.onrender.com/ai/bard"
+
+def fetch_data(api_url: str, query: str) -> tuple:
+    try:
+        response = requests.get(f"{api_url}/{query}")
+        response.raise_for_status()
+        data = response.json()
+        return data.get("content", "No response from the API."), data.get("images", False)
+    except requests.exceptions.RequestException as e:
+        return None, f"Request error: {e}"
+    except Exception as e:
+        return None, f"An error occurred: {str(e)}"
+
+@app.on_message(filters.command(["tchat", "task", "gput"]))
+async def chatgptt(_, message):
+    if len(message.command) < 2:
+        return await message.reply_text("**Please provide a query.**")
+
+    query = " ".join(message.command[1:])    
+    txt = await message.reply_text("**Wait patiently, requesting to API...**")
+    await txt.edit("💭")
+    api_response, error_message = fetch_data(api_url_gpt, query)
+    await txt.edit(api_response or error_message)
+
+
+
+
+
+@app.on_message(filters.command(["tbard", "gemini"]))
+async def bardb(_, message):
+    chat_id = message.chat.id
+    message_id = message.id
+    
+    if len(message.command) < 2:
+        return await message.reply_text("Please provide a query.")
+
+    query = " ".join(message.command[1:])
+    txt = await message.reply_text("Wait patiently, requesting to API...")
+    await txt.edit("💭")
+    
+    api_response, images = fetch_data(api_url_bard, query)
+
+    medias = []
+    bard = str(api_response)
+    try:
+       photo_url = images[-1]
+    except:
+        pass
+    
+    
+    if images:
+        if len(images) > 1:
+            for url in images:
+                medias.append(InputMediaPhoto(media=url, caption=None))
+                        
+            medias[-1] = InputMediaPhoto(media=photo_url, caption=bard)
+            
+            try:
+                await app.send_media_group(chat_id=chat_id, media=medias, reply_to_message_id=message_id)
+                return await txt.delete()
+            except Exception as e:
+                return await txt.edit(str(e))
+        elif len(images) < 2:
+            image_url = images[0]
+            try:
+                await message.reply_photo(photo=image_url, caption=bard)
+                return await txt.delete()
+            except MediaCaptionTooLong:
+                return await txt.edit(bard)
+            except Exception as e:
+                return await txt.edit(str(e))
+        else:
+            return await txt.edit('Somthing went wrong')
+    else:
+        try:
+            return await txt.edit(bard)
+        except Exception as e:
+            return await txt.edit(str(e))
+
+
 # ----------------------------------------
 @app.on_message(filters.command(["chatgpt","ai","ask"],  prefixes=["+", ".", "/", "-", "?", "$","#","&"]))
 async def chat(app :app, message):
