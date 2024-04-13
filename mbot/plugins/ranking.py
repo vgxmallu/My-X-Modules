b2206a110c3238451c4073c171ae984e0bb3d684
VGX.LEO <119828208+Hydra-sjz@users.noreply.github.com>
2024-04-13 20:04:25 +0530

    Update ranking.py

--- mbot/plugins/ranking.py
+++ mbot/plugins/ranking.py
@@ -31,24 +31,20 @@ user_data = {}
 today = {}
 
 MISHI = [
-    "https://graph.org/file/f86b71018196c5cfe7344.jpg",
-    "https://graph.org/file/a3db9af88f25bb1b99325.jpg",
-    "https://graph.org/file/5b344a55f3d5199b63fa5.jpg",
-    "https://graph.org/file/84de4b440300297a8ecb3.jpg",
-    "https://graph.org/file/84e84ff778b045879d24f.jpg",
-    "https://graph.org/file/a4a8f0e5c0e6b18249ffc.jpg",
-    "https://graph.org/file/ed92cada78099c9c3a4f7.jpg",
-    "https://graph.org/file/d6360613d0fa7a9d2f90b.jpg"
-    "https://graph.org/file/37248e7bdff70c662a702.jpg",
-    "https://graph.org/file/0bfe29d15e918917d1305.jpg",
-    "https://graph.org/file/16b1a2828cc507f8048bd.jpg",
-    "https://graph.org/file/e6b01f23f2871e128dad8.jpg",
-    "https://graph.org/file/cacbdddee77784d9ed2b7.jpg",
-    "https://graph.org/file/ddc5d6ec1c33276507b19.jpg",
-    "https://graph.org/file/39d7277189360d2c85b62.jpg",
-    "https://graph.org/file/5846b9214eaf12c3ed100.jpg",
-    "https://graph.org/file/ad4f9beb4d526e6615e18.jpg",
-    "https://graph.org/file/3514efaabe774e4f181f2.jpg",
+    "https://telegra.ph/file/059611caea9ef749feb5d.jpg",
+    "https://telegra.ph/file/b198b080d7a6408ff340b.jpg",
+    "https://telegra.ph/file/b5b64413f6235e4beaafe.jpg",
+    "https://telegra.ph/file/0e6924b678e0e92a62370.jpg",
+    "https://telegra.ph/file/91e4f4082c61318f9eb67.jpg",
+    "https://telegra.ph/file/22c20f5256dea9006111a.jpg",
+    "https://telegra.ph/file/f333abc3500868b56cc99.jpg",
+    "https://telegra.ph/file/890802c4d2ac2c6a0a8af.jpg"
+    "https://telegra.ph/file/f60186ce2c83dc5c98948.jpg",
+    "https://telegra.ph/file/16f957e795a6d3e35a87c.jpg",
+    "https://telegra.ph/file/2348c377ed47034ab38a8.jpg",
+    "https://telegra.ph/file/2720c796a1a27eb018e3d.jpg",
+    "https://telegra.ph/file/d7d665e0785f2681ca35b.jpg",
+    "https://telegra.ph/file/e1f5f90c52cd2e568f699.jpg",
 ]
 
 
