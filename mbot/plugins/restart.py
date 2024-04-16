5b17c1ea2bb58f871d7c586714052fd810a74c53
VGX.LEO <119828208+Hydra-sjz@users.noreply.github.com>
2024-04-16 23:00:44 +0530

    Create restart.py

---
+++ mbot/plugins/restart.py
@@ -0,0 +1,57 @@
+"""MIT License
+
+Copyright (c) 2022 Daniel
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+"""
+
+from datetime import datetime
+from os import execvp, sys
+
+from pyrogram import filters
+from pyrogram.raw.functions import Ping
+from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup
+
+from mbot import AUTH_CHATS, LOG_GROUP, OWNER_ID, SUDO_USERS, Mbot
+
+
+@Mbot.on_message(
+    filters.command("restart") & filters.chat(OWNER_ID) & filters.private
+)
+async def restart(_, message):
+    await message.delete()
+    execvp(sys.executable, [sys.executable, "-m", "mbot"])
+    await message.delete()
+
+
+@Mbot.on_message(filters.command("log") & filters.chat(SUDO_USERS))
+async def send_log(_, message):
+    await message.reply_document("bot.log")
+    await message.delete()
+
+@Mbot.on_message(filters.command("ping"))
+async def pnsing(client, message):
+    start = datetime.now()
+    await client.send(Ping(ping_id=0))
+    ms = (datetime.now() - start).microseconds / 1000
+    await message.reply_text(f"**Pong!**\nResponse time: `{ms} ms`")
+
+
+
+
