37ffdd35407b9e2cb4b05368eea1dea7cbe7bb0a
VGX.LEO <119828208+Hydra-sjz@users.noreply.github.com>
2024-04-21 10:29:46 +0530

    Update db.py

--- mbot/utils/anib/db.py
+++ mbot/utils/anib/db.py
@@ -15,12 +15,12 @@ print("Connecting to Database ...")
 _MGCLIENT: AgnosticClient = AsyncIOMotorClient(DB_URL)
 _RUN = asyncio.get_event_loop().run_until_complete
 
-if "anibot" in _RUN(_MGCLIENT.list_database_names()):
+if "mbot" in _RUN(_MGCLIENT.list_database_names()):
     print("anibot Database Found :) => Now Logging to it...")
 else:
     print("anibot Database Not Found :( => Creating New Database...")
 
-_DATABASE: AgnosticDatabase = _MGCLIENT["anibot"]
+_DATABASE: AgnosticDatabase = _MGCLIENT["mbot"]
 
 
 def get_collection(name: str) -> AgnosticCollection:
