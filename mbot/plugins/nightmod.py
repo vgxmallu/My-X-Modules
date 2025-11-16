a7a9381c9ac7abd6547f65c1d2cbb980e7360854
vgxmallu <DeltaAlgor@outlook.com>
2025-11-16 10:42:16 +0530

    Update nightmod.py

--- mbot/plugins/nightmod.py
+++ mbot/plugins/nightmod.py
@@ -8,26 +8,40 @@ from mbot.utils.Database.nightdb import nightdb,nightmode_on,nightmode_off,get_n
 
 
 
+# To restrict all permissions
 CLOSE_CHAT = ChatPermissions(
     can_send_messages=False,
-    can_send_media_messages = False,
-    can_send_other_messages = False,
-    can_send_polls = False,
-    can_change_info = False,
-    can_add_web_page_previews = False,
-    can_pin_messages = False,
-    can_invite_users = False )
+    can_send_audios=False,
+    can_send_documents=False,
+    can_send_photos=False,
+    can_send_videos=False,
+    can_send_video_notes=False,
+    can_send_voice_notes=False,
+    can_send_other_messages=False,
+    can_send_polls=False,
+    can_change_info=False,
+    can_add_web_page_previews=False,
+    can_pin_messages=False,
+    can_invite_users=False
+)
 
-
+# To grant all permissions
 OPEN_CHAT = ChatPermissions(
     can_send_messages=True,
-    can_send_media_messages = True,
-    can_send_other_messages = True,
-    can_send_polls = True,
-    can_change_info = True,
-    can_add_web_page_previews = True,
-    can_pin_messages = True,
-    can_invite_users = True )
+    can_send_audios=True,
+    can_send_documents=True,
+    can_send_photos=True,
+    can_send_videos=True,
+    can_send_video_notes=True,
+    can_send_voice_notes=True,
+    can_send_other_messages=True,
+    can_send_polls=True,
+    can_change_info=True,
+    can_add_web_page_previews=True,
+    can_pin_messages=True,
+    can_invite_users=True
+)
+
     
 buttons = InlineKeyboardMarkup([[InlineKeyboardButton("๏ ᴇɴᴀʙʟᴇ ๏", callback_data="add_night"),InlineKeyboardButton("๏ ᴅɪsᴀʙʟᴇ ๏", callback_data="rm_night")]])         
 
